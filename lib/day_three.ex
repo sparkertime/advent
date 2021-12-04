963758aa02a4514b64f71cbfa729095ad6a5d3f7
sparker <scott.p.parker@gmail.com>
2021-12-03 22:28:50 -0600

    Day Three Gold

--- lib/day_three.ex
+++ lib/day_three.ex
@@ -1,4 +1,61 @@
 defmodule DayThree do
+  def find_ratings(file) do
+    input =
+      File.stream!(file)
+      |> Enum.map(&String.trim/1)
+
+    {oxygen, _} =
+      input
+      |> find_rating_in_input(fn zeroes, ones ->
+        cond do
+          zeroes > ones -> "0"
+          true -> "1"
+        end
+      end)
+      |> Integer.parse(2)
+
+    {scrubber, _} =
+      input
+      |> find_rating_in_input(fn zeroes, ones ->
+        cond do
+          zeroes > ones -> "1"
+          true -> "0"
+        end
+      end)
+      |> Integer.parse(2)
+
+    %{oxygen: oxygen, scrubber: scrubber}
+  end
+
+  def find_rating_in_input([{result, _}], _eval_fn), do: result
+
+  def find_rating_in_input([{_original_str, _remainder_str} | _] = input, eval_fn) do
+    {zeroes, ones} =
+      input
+      |> Enum.reduce({0, 0}, fn
+        {_, "0" <> _}, {zeroes, ones} -> {zeroes + 1, ones}
+        {_, "1" <> _}, {zeroes, ones} -> {zeroes, ones + 1}
+      end)
+
+    match_char = eval_fn.(zeroes, ones)
+
+    input
+    |> Enum.filter(fn {_original, remainder} ->
+      String.at(remainder, 0) == match_char
+    end)
+    |> Enum.map(fn {original, remainder} ->
+      {_, new_remainder} = String.split_at(remainder, 1)
+      {original, new_remainder}
+    end)
+    |> find_rating_in_input(eval_fn)
+  end
+
+  def find_rating_in_input(input, eval_fn) do
+    input
+    |> Enum.map(&{&1, &1})
+    |> find_rating_in_input(eval_fn)
+  end
+
   def find_rates(file) do
     input = File.stream!(file)
 
--- test/day_three_test.exs
+++ test/day_three_test.exs
@@ -10,4 +10,14 @@ defmodule DayThreeTest do
     x = DayThree.find_rates("./inputs/day_three.txt")
     IO.puts(x.epsilon * x.gamma)
   end
+
+  test "solves the gold example" do
+    assert %{oxygen: 23, scrubber: 10} = DayThree.find_ratings("./inputs/day_three_example.txt")
+  end
+
+  test "calculated the gold answer" do
+    x = DayThree.find_ratings("./inputs/day_three.txt")
+
+    IO.puts(x.oxygen * x.scrubber)
+  end
 end
