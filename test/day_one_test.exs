a1a2f031b11bd88bb6b488e8002bcbf9cbecd767
sparker <scott.p.parker@gmail.com>
2021-12-03 20:39:16 -0600

    Moved inputs outside of lib

--- test/day_one_test.exs
+++ test/day_one_test.exs
@@ -3,18 +3,18 @@ defmodule DayOneTest do
   doctest DayOne
 
   test "parses the example" do
-    assert DayOne.increases("./lib/inputs/day_one_example.txt") == 7
+    assert DayOne.increases("./inputs/day_one_example.txt") == 7
   end
 
   test "it works on the silver test" do
-    IO.inspect(DayOne.increases("./lib/inputs/day_one.txt"))
+    IO.inspect(DayOne.increases("./inputs/day_one.txt"))
   end
 
   test "finds summed increases" do
-    assert DayOne.summed_increases("./lib/inputs/day_one_example.txt") == 5
+    assert DayOne.summed_increases("./inputs/day_one_example.txt") == 5
   end
 
   test "it works on the gold test" do
-    IO.inspect(DayOne.summed_increases("./lib/inputs/day_one.txt"))
+    IO.inspect(DayOne.summed_increases("./inputs/day_one.txt"))
   end
 end
--- test/day_two_test.exs
+++ test/day_two_test.exs
@@ -3,20 +3,20 @@ defmodule DayTwoTest do
   doctest DayTwo
 
   test "parses inputs" do
-    assert DayTwo.parse_commands("./lib/inputs/day_two_example.txt") == %{h: 15, d: 10}
+    assert DayTwo.parse_commands("./inputs/day_two_example.txt") == %{h: 15, d: 10}
   end
 
   test "it solves the problems" do
-    x = DayTwo.parse_commands("./lib/inputs/day_two.txt")
+    x = DayTwo.parse_commands("./inputs/day_two.txt")
     IO.puts(x.d * x.h)
   end
 
   test "parses gold inputs" do
-    assert %{h: 15, d: 60} = DayTwo.parse_gold_commands("./lib/inputs/day_two_example.txt")
+    assert %{h: 15, d: 60} = DayTwo.parse_gold_commands("./inputs/day_two_example.txt")
   end
 
   test "it solves the gold problems" do
-    x = DayTwo.parse_gold_commands("./lib/inputs/day_two.txt")
+    x = DayTwo.parse_gold_commands("./inputs/day_two.txt")
 
     IO.puts(x.d * x.h)
   end
